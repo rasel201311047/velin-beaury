ee86209bcb50df99bc7de1d92786477d3cdffb14
rasel201311047 <mdraselbabu0367@gmail.com>
2025-09-08 07:18:04 +0600

    image done

--- src/components/cart/ServiceCart.jsx
+++ src/components/cart/ServiceCart.jsx
@@ -6,15 +6,23 @@ export default function ServiceCart({ item }) {
     if (!item) return null; 
 
     return (
-        <div className='rounded-lg bg-[#E8E8E9] hover:bg-black h-40 hover:text-white text-[#595959] md:h-28 lg:h-44 w-full overflow-hidden'>
-            <div className='w-full h-3/4 flex justify-center items-center'>
-                {item.image && (
-                    <Image     src={item.image}     alt={item.title}    className=' w-full h-full' />
-                )}
-            </div>
-            <h6 className='text-center  hover:text-white text-sm md:text-xs lg:text-sm font-poppins font-semibold pt-2 mt-1'>
-                {item.title}
-            </h6>
-        </div>
+<div className='rounded-lg bg-[#E8E8E9] hover:bg-black h-40 hover:text-white text-[#595959] md:h-28 lg:h-44 w-full overflow-hidden'>
+  <div className='w-full h-[85%] flex justify-center items-center overflow-hidden'>
+    {item.image && (
+      <Image
+        src={item.image}
+        alt={item.title}
+        className='w-full h-full object-cover'
+      />
+    )}
+  </div>
+  <div className="h-[15%] flex justify-center items-center"> 
+    <h6 className='text-center hover:text-white text-sm md:text-xs lg:text-sm font-poppins font-semibold'>
+      {item.title}
+    </h6>
+  </div>
+</div>
+
+
     )
 }
